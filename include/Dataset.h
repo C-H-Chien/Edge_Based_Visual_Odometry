02c95f6d7ccb46e60b87f3d2332cb21add2ccaef
Saul Lopez Lucas <saullopezlucas@gmail.com>
2025-04-15 11:03:36 -0400

    ncc threshold of 0.65 is producing 99% RR

--- include/Dataset.h
+++ include/Dataset.h
@@ -33,6 +33,7 @@ struct RecallMetrics {
     double epi_shift_recall;
     double epi_cluster_recall;
     double patch_recall;
+    double ncc_recall;
 };
 
 extern cv::Mat merged_visualization_global;
